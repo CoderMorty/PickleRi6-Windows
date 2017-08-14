5767d4d4e9802b0fd964e2cdea5cad2ec903f58f
Bourbbbon <oliver.humphrey@hotmail.co.uk>
2017-08-14 17:54:52 +0100

    This isn't Mee6 +Made it R&M themed.
    Added some responses to what I think rick would say and changed the link to the Mee6 source-code to mine.

--- chat-bot/plugins/git.py
+++ chat-bot/plugins/git.py
@@ -17,16 +17,16 @@ class Git(Plugin):
     async def on_message(self, message):
 
         flavortext = [
-            "Glad you asked!",
-            "Hey kid, wanna see some source code?",
-            "Thanks for stopping by!",
-            "Here's where the magic happens."
+            "Wubba lubba dub dub! Here is my source-code!",
+            "Make sure you grab me the screwdriver while you're in there.",
+            "Don't mess anything up.",
+            "If you must know, here is my source-code."
             ]
 
         if message.content == '!git':
             await self.mee6.send_message(
                 message.channel,
-                '{}\nhttps://github.com/cookkkie/mee6/'.format(
+                '{}\nhttps://github.com/Bourbbbon/PickleRi6/'.format(
                 random.choice(flavortext)
                 )
              )
